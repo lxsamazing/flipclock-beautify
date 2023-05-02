29f382dd03ee66f0a0a720e457f6655ff1a0e720
lxsamazing <1412661856@qq.com>
2023-05-02 11:37:29 +0800

    Update myClockFunctions.js

--- js/myClockFunctions.js
+++ js/myClockFunctions.js
@@ -624,12 +624,11 @@
     var dateString = d.getFullYear() + "年" +(d.getMonth() + 1) + "月" + d.getDate() + "日" + ' 星期'+'日一二三四五六'.charAt(new Date().getDay());
     const axios = require('axios');
         // 调用 API
-           axios.get('https://api.suyanw.cn/api/yiyan?type=json')
+           axios.get('https://api.suyanw.cn/api/yiyan')
              .then(function (response) {
             // 解析 JSON 数据并获取对应值
              const data = JSON.parse(response.data);
-             const text = data.text;
-             console.log(text);
+             console.log(data);
              })
              .catch(function (error) {
              console.log(error);
@@ -638,7 +637,7 @@
         // strings: ["欢迎来到flipclock", "今天也要元气满满哦！", dateString],
         // strings: ["欢迎来到flipclock", "请按下1来设置日期样式"],
           
-        strings: ["text", dateString],
+        strings: ["data", dateString],
         typeSpeed: 80
     });
 
